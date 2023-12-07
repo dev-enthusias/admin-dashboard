8311e338da5df66c74d08b0350639ffb0f044208
Anakor Joshua <backend.mern@gmail.com>
2023-12-07 18:00:37 +0100

    Change layout of the Add New User Form

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-'use strict'
+'use strict';
 
-//Toggle modal
-const addUserForm = document.querySelector('.ad-new-user__form');
-const toggleFormBtn = document.querySelector('.ad-btn-add')
+function app() {
+  // Elements
+  const addUserForm = document.querySelector('.ad-new-user__form');
+  const overlay = document.querySelector('.overlay');
+  const btnOpen = document.querySelector('.ad-btn-add');
+  const btnClose = document.querySelector('.ad-btn-close');
 
-toggleFormBtn.addEventListener('click', function() {
-    addUserForm.classList.toggle('hidden');
-})
+  function openAddNewUserForm() {
+    addUserForm.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  }
+
+  function closeAddNewUserForm() {
+    addUserForm.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+
+  // Events
+  btnOpen.addEventListener('click', openAddNewUserForm);
+  btnClose.addEventListener('click', closeAddNewUserForm);
+}
+app();
